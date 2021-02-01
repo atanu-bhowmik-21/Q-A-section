a1e57cca20560a087c753d34e2f37b251dfecebd
atanu-bhowmik-21 <62740377+atanu-bhowmik-21@users.noreply.github.com>
2021-02-01 10:44:01 +0530

    Add files via upload

---
+++ setup/app.js
@@ -0,0 +1,19 @@
+//using selectors inside the element
+const questions = document.querySelectorAll('.question');
+questions.forEach(function(ques){
+    const btn = ques.querySelector('.question-btn');
+    // console.log(btn);
+    btn.addEventListener('click',function(){
+        ques.classList.toggle('show-text');
+    });
+});
+
+
+// traversing the dom
+// const btns = document.querySelectorAll('.question-btn');
+// btns.forEach(function(btn){
+//     btn.addEventListener('click',function(e){
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
